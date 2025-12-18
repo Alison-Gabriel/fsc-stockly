31c2feed0cc476ef253ca8a523802176eeffb740
Alison Gabriel <odevalison@gmail.com>
2025-12-18 20:30:57 -0300

    feat: update stock badges colors

--- app/products/_components/table-columns.tsx
+++ app/products/_components/table-columns.tsx
@@ -5,10 +5,7 @@ import { Product } from "@/app/_generated/prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
 import { CircleIcon } from "lucide-react";
 import ProductActionsDropdownMenu from "./actions-dropdown-menu";
-
-const getStatusLabel = (status: string) => {
-  return status === "IN_STOCK" ? "Em estoque" : "Esgotado";
-};
+import { formatNumberToBRL } from "@/app/_helpers/number-to-brl";
 
 export const productsColumns: ColumnDef<Product>[] = [
   {
@@ -20,10 +17,7 @@ export const productsColumns: ColumnDef<Product>[] = [
     header: "Valor unitário",
     cell: ({ row }) => {
       const product = row.original;
-      return new Intl.NumberFormat("pt-BR", {
-        style: "currency",
-        currency: "BRL",
-      }).format(Number(product.price));
+      return formatNumberToBRL(Number(product.price));
     },
   },
   {
@@ -35,15 +29,25 @@ export const productsColumns: ColumnDef<Product>[] = [
     header: "Status",
     cell: ({ row }) => {
       const status = row.getValue("status");
-      const label = getStatusLabel(status);
+
+      const getStatusLabel = (status: "IN_STOCK" | "OUT_OF_STOCK") => {
+        if (status === "IN_STOCK") {
+          return "Em estoque";
+        }
+        return "Fora de estoque";
+      };
+
+      const statusLabel = getStatusLabel(status);
+
       return (
         <Badge
-          className={`${status === "OUT_OF_STOCK" ? "bg-slate-50 text-slate-500" : "bg-emerald-50 text-emerald-500"} gap-[4.5px] font-semibold`}
+          variant={`${status === "OUT_OF_STOCK" ? "secondary" : "default"}`}
+          className={`gap-[4.5px] font-semibold`}
         >
           <CircleIcon
-            className={`${status === "OUT_OF_STOCK" ? "fill-slate-500" : "fill-emerald-500"} size-2!`}
+            className={`${status === "OUT_OF_STOCK" ? "fill-secondary-foreground" : "fill-primary-foreground"} size-2!`}
           />
-          {label}
+          {statusLabel}
         </Badge>
       );
     },
