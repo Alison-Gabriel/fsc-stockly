0e38c4077278a35f16194f33b4fa2b0e2965ce54
Alison Gabriel <odevalison@gmail.com>
2025-12-18 18:53:37 -0300

    feat: ensure that products stock is updated when a sale is made

--- app/_actions/sale/create-sale/index.ts
+++ app/_actions/sale/create-sale/index.ts
@@ -7,44 +7,47 @@ import { revalidatePath } from "next/cache";
 export const createSale = async ({ products }: CreateSaleSchema) => {
   createSaleSchema.parse({ products });
 
-  const sale = await db.sale.create({
-    data: {
-      date: new Date(),
-    },
-  });
-
-  for (const product of products) {
-    const productFromDb = await db.product.findUnique({
-      where: { id: product.id },
-    });
-
-    const isProductNotFounded = !productFromDb;
-    if (isProductNotFounded) {
-      throw new Error("Product not found");
-    }
-
-    const isProductOutOfStock = product.quantity > productFromDb.stock;
-    if (isProductOutOfStock) {
-      throw new Error("Product is out of stock");
-    }
-
-    await db.saleProduct.create({
+  await db.$transaction(async (transaction) => {
+    const sale = await transaction.sale.create({
       data: {
-        saleId: sale.id,
-        productId: product.id,
-        quantity: product.quantity,
-        unitPrice: productFromDb.price,
+        date: new Date(),
       },
     });
-    await db.product.update({
-      where: { id: product.id },
-      data: {
-        stock: {
-          decrement: product.quantity,
+
+    for (const product of products) {
+      const productFromDb = await db.product.findUnique({
+        where: { id: product.id },
+      });
+
+      const isProductNotFounded = !productFromDb;
+      if (isProductNotFounded) {
+        throw new Error("Product not found");
+      }
+
+      const isProductOutOfStock = product.quantity > productFromDb.stock;
+      if (isProductOutOfStock) {
+        throw new Error("Product is out of stock");
+      }
+
+      await transaction.saleProduct.create({
+        data: {
+          saleId: sale.id,
+          productId: product.id,
+          quantity: product.quantity,
+          unitPrice: productFromDb.price,
         },
-      },
-    });
-  }
+      });
+
+      await transaction.product.update({
+        where: { id: product.id },
+        data: {
+          stock: {
+            decrement: product.quantity,
+          },
+        },
+      });
+    }
+  });
 
   revalidatePath("/products");
 };
