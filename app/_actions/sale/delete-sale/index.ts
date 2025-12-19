d2017370703c12f9d6fcbea855a4e210fa2c4fee
Alison Gabriel <odevalison@gmail.com>
2025-12-19 12:40:49 -0300

    feat: ensure that product stock is restored after a sale deletion

--- app/_actions/sale/delete-sale/index.ts
+++ app/_actions/sale/delete-sale/index.ts
@@ -4,12 +4,45 @@ import { actionClient } from "@/app/_lib/safe-action";
 import { deleteSaleSchema } from "./schema";
 import { db } from "@/app/_lib/prisma";
 import { revalidatePath } from "next/cache";
+import { returnValidationErrors } from "next-safe-action";
 
 export const deleteSale = actionClient
   .inputSchema(deleteSaleSchema)
   .action(async ({ parsedInput: { id } }) => {
-    await db.sale.delete({
-      where: { id },
+    await db.$transaction(async (transaction) => {
+      const saleToDelete = await db.sale.findUnique({
+        where: { id },
+        include: {
+          saleProducts: true,
+        },
+      });
+      const hasNotFoundSaleToDelete = !saleToDelete;
+
+      if (hasNotFoundSaleToDelete) {
+        returnValidationErrors(deleteSaleSchema, {
+          _errors: ["Sale to delete has not found"],
+        });
+      }
+
+      for (const saleProduct of saleToDelete.saleProducts) {
+        const { quantity, productId } = saleProduct;
+
+        await transaction.product.update({
+          where: { id: productId },
+          data: {
+            stock: {
+              increment: quantity,
+            },
+          },
+        });
+      }
+
+      await transaction.sale.delete({
+        where: { id: saleToDelete.id },
+      });
     });
+
     revalidatePath("/sales");
+    revalidatePath("/products");
+    revalidatePath("/");
   });
